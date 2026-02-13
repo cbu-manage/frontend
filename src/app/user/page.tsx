75b3fd5df7dbfa2356ad60d0e7c303c9e7b7425e
ming0o <isha3992@gmail.com>
2026-02-13 23:57:35 +0900

    Fix: 사용자 페이지 동적 렌더링 변경

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+// /user 페이지는 로그인 상태 등 런타임 정보에 의존하므로
+// 정적 프리렌더링 대신 항상 동적 렌더링하도록 설정
+export const dynamic = "force-dynamic";
+
 import { useUserStore } from "@/store/userStore";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
