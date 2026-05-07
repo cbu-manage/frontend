9207714f6f440d6492490cfcfea4e0011afff10a
ryeoeunn <fudms@tukorea.ac.kr>
2026-05-07 17:34:18 +0900

    feat: 서버 에러 메시지를 에러 페이지에 그대로 노출

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 import ErrorFallback from "@/components/common/ErrorFallback";
 
 export default function NotFound() {
-  return <ErrorFallback status={404} />;
+  return (
+    <ErrorFallback message="주소가 잘못되었거나 삭제된 페이지예요." />
+  );
 }
